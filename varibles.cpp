8861f4864b0a807bec6a961a37c77c28f65c3dfe
SWASTIKGIT-debug <swastikchatterjee550@gmail.com>
2024-10-05 19:38:00 +0530

    changed vars

--- varibles.cpp
+++ varibles.cpp
@@ -1,6 +1,6 @@
 //printing 1 to 5 numbers using static variables.
 #include<stdio.h>
-int loop()//creting a function
+int loop()//creting a function;
 {
 	static int a=1;
 	printf("the numbers are=%d",a);
